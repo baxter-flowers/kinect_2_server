95fc0486a215076780d2f67f2a2e6f017d00ee44
Yoan Mollard <yoan.mollard@inria.fr>
2016-04-12 18:06:54 +0200

    Upload speech recognition draft

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,222 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="MainWindow.xaml.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace Microsoft.Samples.Kinect.SpeechBasics
+{
+    using System;
+    using System.Xml;
+    using System.Collections.Generic;    
+    using System.ComponentModel;
+    using System.IO;
+    using System.Runtime.InteropServices;
+    using System.Text;    
+    using System.Windows;    
+    using System.Windows.Documents;
+    using System.Windows.Media;
+    using Microsoft.Kinect;    
+    using Microsoft.Speech.AudioFormat;
+    using Microsoft.Speech.Recognition;
+    using Microsoft.Win32;
+    using System.Collections;
+
+ 
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1001:TypesThatOwnDisposableFieldsShouldBeDisposable",
+        Justification = "In a full-fledged application, the SpeechRecognitionEngine object should be properly disposed. For the sake of simplicity, we're omitting that code in this sample.")]
+    public partial class MainWindow : Window
+    {
+        private const string MediumGreyBrushKey = "MediumGreyBrush";
+        private KinectSensor kinectSensor = null;
+        private KinectAudioStream convertStream = null;
+        private SpeechRecognitionEngine speechEngine = null;
+        private static string fileName = null;
+        private static string location = "C:/Users/Yoan/Documents/Internship Lucas/SpeechBasics-WPF-Lucas/SpeechGrammar-en-US.xml";
+
+
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private void getXMLGrammarFile(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog
+            OpenFileDialog dlg = new OpenFileDialog();
+
+            // Set filter for file extension and default file extension
+            dlg.DefaultExt = ".xml";
+            dlg.Filter = "XML Files (.xml)|*.xml";
+
+            // Display OpenFileDialog by calling ShowDialog method
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Get the selected file path and set it in location
+            if (result == true)
+            {
+                location = dlg.FileName;
+                fileName = dlg.SafeFileName;
+
+                switch(fileName)
+                {
+                    case "SpeechGrammar-en-US.xml":
+                        language.Text = "Current language : English";
+                        break;
+                    case "SpeechGrammar-fr-FR.xml":
+                        language.Text = "Current language : French";
+                        break;
+                }
+            }
+        }
+
+       
+        private static RecognizerInfo TryGetKinectRecognizer()
+        {
+            IEnumerable<RecognizerInfo> recognizers;
+            
+            // This is required to catch the case when an expected recognizer is not installed.
+            // By default - the x86 Speech Runtime is always expected. 
+            try
+            {
+                recognizers = SpeechRecognitionEngine.InstalledRecognizers();
+            }
+            catch (COMException)
+            {
+                return null;
+            }
+            
+            //Load the xml file 
+            XmlDocument grammar = new XmlDocument();
+            grammar.Load(location);
+
+            //Convert the xml file into string
+            StringWriter sw = new StringWriter();
+            XmlTextWriter tw = new XmlTextWriter(sw);
+            grammar.WriteTo(tw);
+            string grammarText = sw.ToString();
+            
+            //Create the XmlNamespaceManager.
+            NameTable nt = new NameTable();
+            XmlNamespaceManager nsmgr = new XmlNamespaceManager(nt);
+
+            //Create the XmlParserContext.
+            XmlParserContext context = new XmlParserContext(null, nsmgr, null, XmlSpace.None);
+
+            //Create the reader.
+            XmlTextReader reader = new XmlTextReader(grammarText, XmlNodeType.Element, context);
+            reader.WhitespaceHandling = WhitespaceHandling.None;
+            
+            //Get the language
+            reader.Read();
+            string language = reader.XmlLang;
+            reader.Close();
+
+            foreach (RecognizerInfo recognizer in recognizers)
+            {
+                string value;
+                recognizer.AdditionalInfo.TryGetValue("Kinect", out value);
+                if ("True".Equals(value, StringComparison.OrdinalIgnoreCase) && language.Equals(recognizer.Culture.Name, StringComparison.OrdinalIgnoreCase))
+                {
+                    return recognizer;
+                }
+            }
+
+            return null;
+        }
+
+        private void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+            // Only one sensor is supported
+            this.kinectSensor = KinectSensor.GetDefault();
+
+            if (this.kinectSensor != null)
+            {
+                // open the sensor
+                this.kinectSensor.Open();
+
+                // grab the audio stream
+                IReadOnlyList<AudioBeam> audioBeamList = this.kinectSensor.AudioSource.AudioBeams;
+                System.IO.Stream audioStream = audioBeamList[0].OpenInputStream();
+
+                // create the convert stream
+                this.convertStream = new KinectAudioStream(audioStream);
+            }
+            else
+            {
+                // on failure, set the status text
+                this.statusBarText.Text = Properties.Resources.NoKinectReady;
+                return;
+            }
+
+            RecognizerInfo ri = TryGetKinectRecognizer();
+
+            if (null != ri)
+            {
+                this.speechEngine = new SpeechRecognitionEngine(ri.Id);
+
+                // Create a grammar from grammar definition XML file.
+
+                //Load the xml file 
+                XmlDocument grammar = new XmlDocument();
+                grammar.Load(location);
+
+                //Convert the xml file into string
+                StringWriter sw = new StringWriter();
+                XmlTextWriter tw = new XmlTextWriter(sw);
+                grammar.WriteTo(tw);
+                string grammarText = sw.ToString();
+                using (var memoryStream = new MemoryStream(Encoding.ASCII.GetBytes(grammarText)))
+                {
+                    var g = new Grammar(memoryStream);
+                    this.speechEngine.LoadGrammar(g);
+                }
+
+                //this.speechEngine.SpeechRecognized += this.SpeechRecognized;
+                this.speechEngine.SpeechRecognitionRejected += this.SpeechRejected;
+        
+                // let the convertStream know speech is going active
+                this.convertStream.SpeechActive = true;
+
+                // For long recognition sessions (a few hours or more), it may be beneficial to turn off adaptation of the acoustic model. 
+                // This will prevent recognition accuracy from degrading over time.
+                ////speechEngine.UpdateRecognizerSetting("AdaptationOn", 0);
+
+                this.speechEngine.SetInputToAudioStream(
+                    this.convertStream, new SpeechAudioFormatInfo(EncodingFormat.Pcm, 16000, 16, 1, 32000, 2, null));
+                this.speechEngine.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            else
+            {
+                this.statusBarText.Text = Properties.Resources.NoSpeechRecognizer;
+            }
+        }
+
+        private void WindowClosing(object sender, CancelEventArgs e)
+        {
+            if (null != this.convertStream)
+            {
+                this.convertStream.SpeechActive = false;
+            }
+
+            if (null != this.speechEngine)
+            {
+                //this.speechEngine.SpeechRecognized -= this.SpeechRecognized;
+                this.speechEngine.SpeechRecognitionRejected -= this.SpeechRejected;
+                this.speechEngine.RecognizeAsyncStop();
+            }
+
+            if (null != this.kinectSensor)
+            {
+                this.kinectSensor.Close();
+                this.kinectSensor = null;
+            }
+        }
+
+     
+        private void SpeechRejected(object sender, SpeechRecognitionRejectedEventArgs e)
+        {
+            lastWord.Text = "Last word recognized : ...";
+        }
+    }
+}
