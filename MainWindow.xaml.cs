dec582817afdb7880fc328d3737e7ae1d26a0496
Lucas Gueze <lucas.gueze@gmail.com>
2016-04-14 18:47:53 +0200

    Version with a switch for the KinectSensor, different tabs and you can fix your own confidence

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -4,73 +4,140 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Linq;
 using System.Windows;
 using Microsoft.Kinect;    
 using Microsoft.Speech.AudioFormat;
 using Microsoft.Speech.Recognition;
-using Microsoft.Win32;
 using System.Collections;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 
 namespace Kinect2Server
 {
      public partial class MainWindow : Window
     {
-        private const string MediumGreyBrushKey = "MediumGreyBrush";
-        private KinectSensor kinectSensor = null;
-        private KinectAudioStream convertStream = null;
-        private Grammar grammar = null;
-        private SpeechRecognitionEngine speechEngine = null;
-        private static string grammarText = null;
-        private static string fileName = null;
-        private static string location = "C:/Users/Yoan/Documents/Internship Lucas/SpeechBasics-WPF-Lucas/testGrammar.xml";
-        private static string currentLanguage = null;
-
-
+        
         public MainWindow()
         {
             this.InitializeComponent();
         }
 
-        // Update the XML file when the user opens a file
-        private void updateXMLGrammarFile(object sender, RoutedEventArgs e)
+/*---------------------------------------------------------------------------------------
+* 
+*                                WINDOW 
+* 
+*----------------------------------------------------------------------------------------*/
+
+        
+
+        private void WindowLoaded(object sender, RoutedEventArgs e)
         {
-            // Create OpenFileDialog
-            OpenFileDialog dlg = new OpenFileDialog();
-
-            // Set filter for file extension and default file extension
-            dlg.DefaultExt = ".xml";
-            dlg.Filter = "XML Files (.xml)|*.xml";
-
-            // Display OpenFileDialog by calling ShowDialog method
-            Nullable<bool> result = dlg.ShowDialog();
-
-            // Get the selected file path and set it in location if it is different from the actual file
-            if (result == true && location!=dlg.FileName)
-            {
-                location = dlg.FileName;
-                fileName = dlg.SafeFileName;
-                
-            }
-            setGrammarText(location);
-            createGrammar(sender, e);
-            setCurrentLanguage(grammarText);
-
-            switch (currentLanguage)
-                {
-                    case "en-US" :
-                        this.language.Text = Properties.Resources.AmericanEnglish;
-                        break;
-
-                    case "fr-FR" :
-                        this.language.Text = Properties.Resources.French;
-                        break;
-                }
+            
         }
 
-       
-        private static void setGrammarText(string location) {
+        private void WindowClosing(object sender, CancelEventArgs e)
+        {
+            if (null != this.convertStream)
+            {
+                this.convertStream.SpeechActive = false;
+            }
+
+            if (null != this.speechEngine)
+            {
+                this.speechEngine.SpeechRecognized -= this.SpeechRecognized;
+                this.speechEngine.SpeechRecognitionRejected -= this.SpeechRejected;
+                this.speechEngine.RecognizeAsyncStop();
+            }
+
+            if (null != this.kinectSensor)
+            {
+                this.kinectSensor.Close();
+                this.kinectSensor = null;
+            }
+        }
+
+
+/*---------------------------------------------------------------------------------------
+* 
+*                                KINECT'S STATUS
+* 
+*----------------------------------------------------------------------------------------*/
+
+        private KinectSensor kinectSensor = null;
+        private KinectAudioStream convertStream = null;
+
+
+        private void setKinectSensor(object sender, RoutedEventArgs e)
+        {
+            // Only one sensor is supported
+            this.kinectSensor = KinectSensor.GetDefault();
+
+            if (this.kinectSensor != null)
+            {
+                // open the sensor
+                this.kinectSensor.Open();
+
+                // grab the audio stream
+                IReadOnlyList<AudioBeam> audioBeamList = this.kinectSensor.AudioSource.AudioBeams;
+                System.IO.Stream audioStream = audioBeamList[0].OpenInputStream();
+
+                // create the convert stream
+                this.convertStream = new KinectAudioStream(audioStream);
+
+            }
+            else
+            {
+                // on failure, set the status text
+                this.statusBarText.Text = Properties.Resources.NoKinectReady;
+            }
+        }
+
+        // Create, close or open the KinectSensor regarding to its status 
+        private void switchSR(object sender, RoutedEventArgs e)
+        {
+            Image img = new Image();
+            this.stackP.Children.Clear();
+            if (kinectSensor == null)
+            {
+                img.Source = new BitmapImage(new Uri(@"Images/switch_on.png", UriKind.Relative));
+                this.stackP.Children.Add(img);
+                setKinectSensor(sender, e);
+
+                //Enable the Browse button
+                this.browse.IsEnabled = true;
+            }
+            else if (!kinectSensor.IsOpen)
+            {
+                img.Source = new BitmapImage(new Uri(@"Images/switch_on.png", UriKind.Relative));
+                this.stackP.Children.Add(img);
+                kinectSensor.Open();
+            }
+            else
+            {
+                img.Source = new BitmapImage(new Uri(@"Images/switch_off.png", UriKind.Relative));
+                this.stackP.Children.Add(img);
+                this.lastWord.Text = "";
+                kinectSensor.Close();
+            }
+        }
+        
+        
+/*---------------------------------------------------------------------------------------
+ * 
+ *                               SPEECH RECOGNITION
+ * 
+ *---------------------------------------------------------------------------------------*/
+        private Grammar grammar = null;
+        private SpeechRecognitionEngine speechEngine = null;
+        private static string grammarText;
+        private static string fileName;
+        private static string location;
+        private static string currentLanguage;
+        private double confidenceThreshold = 0.3;
+
+
+        private static void setGrammarText(string location)
+        {
             //Load the xml file 
             XmlDocument grammar = new XmlDocument();
             grammar.Load(location);
@@ -81,6 +148,7 @@ namespace Kinect2Server
             grammar.WriteTo(tw);
             grammarText = sw.ToString();
         }
+
 
         private static void setCurrentLanguage(string grammarText)
         {
@@ -104,7 +172,7 @@ namespace Kinect2Server
         private static RecognizerInfo TryGetKinectRecognizer()
         {
             IEnumerable<RecognizerInfo> recognizers;
-            
+
             // This is required to catch the case when an expected recognizer is not installed.
             // By default - the x86 Speech Runtime is always expected. 
             try
@@ -115,7 +183,7 @@ namespace Kinect2Server
             {
                 return null;
             }
-            
+
             setGrammarText(location);
             setCurrentLanguage(grammarText);
 
@@ -130,31 +198,6 @@ namespace Kinect2Server
             }
 
             return null;
-        }
-
-        private void setKinectSensor(object sender, RoutedEventArgs e)
-        {
-            // Only one sensor is supported
-            this.kinectSensor = KinectSensor.GetDefault();
-
-            if (this.kinectSensor != null)
-            {
-                // open the sensor
-                this.kinectSensor.Open();
-
-                // grab the audio stream
-                IReadOnlyList<AudioBeam> audioBeamList = this.kinectSensor.AudioSource.AudioBeams;
-                System.IO.Stream audioStream = audioBeamList[0].OpenInputStream();
-
-                // create the convert stream
-                this.convertStream = new KinectAudioStream(audioStream);
-                
-            }
-            else
-            {
-                // on failure, set the status text
-                this.statusBarText.Text = Properties.Resources.NoKinectReady;
-            }
         }
 
         private void createGrammar(object sender, RoutedEventArgs e)
@@ -193,49 +236,71 @@ namespace Kinect2Server
             }
         }
 
-        private void WindowLoaded(object sender, RoutedEventArgs e)
+
+        // Update the XML file when the user opens a file
+        private void updateXMLGrammarFile(object sender, RoutedEventArgs e)
         {
-            setKinectSensor(sender, e);
-            createGrammar(sender, e);
+            // Create OpenFileDialog
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            // Set filter for file extension and default file extension
+            dlg.DefaultExt = ".xml";
+            dlg.Filter = "XML Files (.xml)|*.xml";
+
+            // Display OpenFileDialog
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Get the selected file path and set it in location if it is different from the actual file
+            if (result == true && location != dlg.FileName)
+            {
+                location = dlg.FileName;
+                fileName = dlg.SafeFileName;
+
+                setGrammarText(location);
+                setCurrentLanguage(grammarText);
+
+                // Create a new grammar for the file loaded
+                createGrammar(sender, e);
+            }
+
+            switch (currentLanguage)
+            {
+                case "en-US":
+                    this.language.Text = Properties.Resources.AmericanEnglish;
+                    break;
+
+                case "fr-FR":
+                    this.language.Text = Properties.Resources.French;
+                    break;
+            }
         }
 
-        private void WindowClosing(object sender, CancelEventArgs e)
+        private void submitConfidence(object sender, RoutedEventArgs e)
         {
-            if (null != this.convertStream)
-            {
-                this.convertStream.SpeechActive = false;
-            }
-
-            if (null != this.speechEngine)
-            {
-                this.speechEngine.SpeechRecognized -= this.SpeechRecognized;
-                this.speechEngine.SpeechRecognitionRejected -= this.SpeechRejected;
-                this.speechEngine.RecognizeAsyncStop();
-            }
-
-            if (null != this.kinectSensor)
-            {
-                this.kinectSensor.Close();
-                this.kinectSensor = null;
-            }
+            this.confidenceThreshold = (double)this.upDown.Value/100;
         }
 
         private void SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
         {
-            // Speech utterance confidence below which we treat speech as if it hadn't been heard
-            const double ConfidenceThreshold = 0.3;
 
-            if (e.Result.Confidence >= ConfidenceThreshold)
+            if (e.Result.Confidence >= confidenceThreshold)
             {
                 this.lastWord.Text = e.Result.Text;
+                /*SemanticValue semantics = e.Result.Semantics;
+                foreach(KeyValuePair<String,SemanticValue> child in semantics) {
+                    this.lastWord.Text += child.Value.ToString();
+                }*/
+
             }
 
 
         }
-     
+
         private void SpeechRejected(object sender, SpeechRecognitionRejectedEventArgs e)
         {
             this.lastWord.Text = Properties.Resources.NoWordsRecognized;
         }
+
+
     }
 }
